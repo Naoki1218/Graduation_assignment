35385471ec0f1daa1b15e03cd55ce791fa790743
Naoki1218 <naoki9890@yahoo.co.jp>
2020-12-18 13:27:04 +0900

    管理者画面日本語翻訳

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
 user = User.new(:name => 'test', :email => 'test@test.com', :password => '11111111')
+user = User.new(:name => 'admin', :email => 'admin@admin.com', :password => '00000000',
+                :admin => 'true')
 user.save!
