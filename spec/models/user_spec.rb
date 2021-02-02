d5e496af283df9d5cbc7c6018735c675a55253db
Naoki1218 <naoki9890@yahoo.co.jp>
2021-02-02 20:24:28 +0900

    model user spec　追加

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  describe 'バリデーションのテスト' do
+      context 'ユーザー登録の際、Eメールが空の場合' do
+        it 'バリデーションにひっかる' do
+
+          user = User.new(email: '', password: '11111111', name: 'test1')
+          expect(user).not_to be_valid
+        end
+      end
+
+      context 'ユーザー登録の際、パスワードが空の場合' do
+        it 'バリデーションにひっかかる' do
+          # ここに内容を記載する
+          user = User.new(email: 'test1@test1.com', password: '', name: 'test1')
+          expect(user).not_to be_valid
+        end
+      end
+
+      context 'ユーザー登録の際、名前が空の場合' do
+        it 'バリデーションにひっかかる' do
+          # ここに内容を記載する
+          user = User.new(email: 'test1@test1.com', password: '11111111', name: '')
+          expect(user).not_to be_valid
+        end
+      end
+
+      context 'ユーザー登録の際、Eメールとパスワードと名前に内容が記載されている場合' do
+        it 'バリデーションが通る' do
+
+          user = User.new(email: 'test1@test1.com', password: '11111111', name: 'test1')
+          expect(user).to be_valid
+        end
+      end
+
+      context 'ユーザー登録の際、Eメールとパスワードと名前と管理者欄に内容が記載され、かつ管理者のチェックボックスにチェックがされている場合' do
+        it 'バリデーションが通る' do
+
+          user = User.new(email: 'admin@admin.com', password: '00000000', name: 'admin', admin: 'true')
+          expect(user).to be_valid
+        end
+      end
+    end
+end
